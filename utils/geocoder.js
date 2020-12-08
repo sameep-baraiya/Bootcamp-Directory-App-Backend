90e152cb056e0b62c4c3fe4701ecc51777102a08
Baraiya Sameep <baraiya.sameep@gmail.com>
2020-12-08 20:53:56 +0530

    geocoder done

---
+++ utils/geocoder.js
@@ -0,0 +1,12 @@
+const NodeGecoder = require('node-geocoder');
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+};
+
+const geocoder = NodeGecoder(options);
+
+module.exports = geocoder;
