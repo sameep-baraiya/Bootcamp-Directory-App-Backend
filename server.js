73de228219e30ee86f0fa96b69d8a85d662dad88
Baraiya Sameep <baraiya.sameep@gmail.com>
2020-12-10 14:55:05 +0530

    Sending JWT in a cookie done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 // Load env vars
@@ -21,6 +22,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // app.use(logger);
 // Dev logging middleware
